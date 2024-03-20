eecfe0bcf0e05383fcc5cba4acdffdb00c08a614
MANISH-RANA229 <manish2031055@akgec.ac.in>
2024-03-21 00:51:28 +0530

    PublishVideo And delete Video

--- src/controllers/Video.controller.js
+++ src/controllers/Video.controller.js
@@ -4,7 +4,27 @@ import {asyncHandler} from "../utills/AsyncHandler.js"
 import {ApiErrors} from "../utills/ApiErrors.js"
 import {ApiResponse} from "../utills/ApiResponse.js"
 import {uploadToCloudinary} from "../utills/Cloudinary.js"
-import {User} from "../models/user.model.js"
+
+
+
+const getPublicid =async(cloudinaryUrl)=>{
+  const urlArray=cloudinaryUrl.split('/');
+  const Image=urlArray[urlArray.length-1];
+  const ImageName=Image.split('.')[0];
+
+  return ImageName;
+}
+
+const DeleteFromCloudinary=async(PublicId)=>{
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    console.log('Deleted resource:', result.result);
+    return result;
+  } catch (errors) {
+    console.error('Error deleting resource:', errors.message);
+    throw new ApiErrors(500,errors, "error while uploadnig in cloudinray");
+  }
+}
 
 
 
@@ -69,7 +89,7 @@ const updateVideo= asyncHandler(async(req,res)=>{
     const { videoId }=req.params;
     const { title, description } = req.body;
     if (!title && !description) {
-        throw new ApiError(400, "All fields are required");
+        throw new ApiErrors(400, "All fields are required");
       }
 
     const videoPath=req.files.videoFile[0].path;
@@ -112,11 +132,79 @@ const updateVideo= asyncHandler(async(req,res)=>{
   
   );
 
-  if (!UpdatedVideo) {
+  if (!UpdatedVideo){
     throw new ApiErrors(500, "error while updating video!");
   }
-return response
+ return response
 .status(200)
 .json(new ApiResponse(200,UpdateVideo,"Video Updated sucessfully"))
 
+})
+
+const deleteVideo= asyncHandler(async(req,res)=>{
+  //get the video ID
+
+
+  const {videoID}= req.params;
+  //find in dataBase
+  const video= await Video.findById(videoID);
+
+  if(!video){
+    throw new ApiErrors(400,"Video does not found")
+  }
+  if(video.owner.toString()!=(req.user._id).toString()){
+    throw new ApiErrors(400,"Unauthorised access while deleting");
+  }
+
+  //delete from cloudinary
+
+   //get public id 
+
+   const thumbnailPublicId= getPublicid(video.thumbnail);
+
+   await DeleteFromCloudinary(thumbnailPublicId);
+
+   const VideoPublicId= getPublicid(video.videoFile);
+
+   await DeleteFromCloudinary(VideoPublicId);
+
+   //delete video
+  
+
+  const DeleteVideo=await Video.findByIdAndDelete(videoID);
+
+  if(!DeleteVideo){
+    throw new ApiErrors(500, "error while Deleting video in DB");
+  }
+
+  //return response
+  return response
+  .status(200)
+  .json(new ApiResponse(200,"video Deleted Successfully"));
+
+
+})
+
+const toggelPublishStatus=asyncHandler(async(req,res)=>{
+
+
+  const VideoId=req.params.id;
+
+  if(!VideoId){
+    throw new ApiErrors(400,"VideoId is require");
+  }
+
+  const video=await Video.find(VideoId);
+
+  if(video.owner.toString()!=(req.user._id).toString()){
+    throw new ApiErrors(400,"Unauthorised User")
+  }
+
+
+  video.isPublished = !(video.isPublished);
+   await video.save();
+
+  return response 
+  .status(200)
+  .json(new ApiResponse(200,"Toggle Succesfully"));
 })
